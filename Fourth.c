a4305fc5308babdbb8b7bafa01ea2712bb06ba6d
deepdwivedi12 <deepdwivedi164@gmail.com>
2025-10-13 08:41:40 +0530

    Create Fourth.c

---
+++ Fourth.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    
+    printf("enter a character:");
+    scanf("%c", &ch);
+    
+    if(ch == 'a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+    ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    {
+        printf("%c is a vowel.\n",ch);
+    }
+    else if((ch >= 'a' && ch <= 'z')||(ch >= 'A' && ch == 'B'))
+    {
+        printf("%c is a consonant.\n",ch);
+    }
+    else
+    {
+        printf("invalid input! please enter an alphabet.\n");
+    }
+    
+    
+    
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
